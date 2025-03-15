6be74a381f44d9e4d189ce01b3aa04c65fbff417
Nick Phillips <nick@nphillips.com>
2025-03-15 10:35:06 -0400

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,16 @@
 import { useState } from "react";
+import { puppyList } from "./data.js";
 import "./App.css";
-import Chase from "./components/Chase";
-
-const dogs = [
-  { id: 1, name: "Logan", breed: "Italian Greyhound", age: 14 },
-  { id: 2, name: "Chase", breed: "Italian Greyhound", age: 12 },
-  { id: 3, name: "Lincoln", breed: "Mixed Rescue", age: 6 },
-];
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [bestDog, setBestDog] = useState(null);
-
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log("puppyList: ", puppyList);
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <Chase />
-
-        {bestDog && <h3>{bestDog} is awesome!</h3>}
-        {dogs.map((dog) => {
-          return (
-            <p key={dog.id} onClick={() => setBestDog(dog.name)}>
-              {dog.name} is a {dog.age} year old {dog.breed}.
-            </p>
-          );
-        })}
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>;
+      })}
+    </div>
   );
 }
 
